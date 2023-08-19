604b502bbc62219b41b8849a184a8e175cbd1901
Juana Solano <juana.solano@correo.tdea.edu.co>
2023-08-19 17:46:07 -0500

    init project

---
+++ App.tsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Dimensions,
+  ImageBackground,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+      <Text>titulo</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App;
