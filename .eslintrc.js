7a5159f8759a039fedcab12303c612d4947f7bf7
cristian.quintero3 <cristian.quintero@pragma.com.co>
2023-08-22 22:16:49 -0500

    fix: resolve eslint and prettier overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    quotes: ['error', 'single'],
+  },
+  'prettier/prettier': [
+    'error',
+    {
+      endOfLine: 'auto',
+    },
+  ],
 };
